1351220a671f3bfda74d8c5ba11de20d75bda26c
Totto16 <tobiausgais@gmail.com>
2023-10-15 22:13:11 +0200

    Port to gnome 45: - refactor everything into class , since the required export format is now a class! - only compatible with gome shell >= 45

--- extension.js
+++ extension.js
@@ -1,223 +1,216 @@
-const Clutter = imports.gi.Clutter;
-const St = imports.gi.St;
-const Main = imports.ui.main;
-// const Tweener = imports.ui.tweener;
-const Gio = imports.gi.Gio;
-const Mainloop = imports.mainloop;
-// const GLib = imports.gi.GLib;
+import { default as Clutter } from 'gi://Clutter';
+import { default as St } from 'gi://St';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { default as Gio } from 'gi://Gio';
+const Mainloop = imports.mainloop; // still using legacy imports (GNOME Shell < 45), but no new method exists for this import!
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
+export default class HardDiskLEDExtension extends Extension {
 
-const PREFS_SCHEMA = 'org.gnome.shell.extensions.harddiskled';
-const refreshTime = 2.0;
+    PREFS_SCHEMA = 'org.gnome.shell.extensions.harddiskled';
+    refreshTime = 2.0;
 
-const ledThreshold = 500000;
-const ledMinThreshold = 100000;
+    ledThreshold = 500000;
+    ledMinThreshold = 100000;
 
-let settings;
-let button, timeout;
-// let icon, iconDark;
-let cur;
-let ioSpeed;
-let lastCount, lastSpeed;
-let mode;
-let layoutManager;
-let ioSpeedStaticIcon;
-let ioSpeedIcon;
-let byteArrayToString;
+    settings = null;
+    button = null
+    timeout = null
+    cur = null
+    ioSpeed = null
+    lastCount = null
+    lastSpeed = null
+    mode = null;
+    layoutManager = null;
+    ioSpeedStaticIcon = null;
+    ioSpeedIcon = null;
+    byteArrayToString = null;
 
-if (global.TextDecoder) {
-    // available in gjs >= 1.70 (GNOME Shell >= 42)
-    byteArrayToString = (new TextDecoder().decode);
-}
-else {
-    // gjs-specific
-    byteArrayToString = imports.byteArray.toString;
-}
+    init() {
+        this.cur = 0;
+        this.lastCount = 0;
 
-function init() {
-    cur = 0;
-    lastCount = 0;
-}
-
-function changeMode() {
-    mode++;
-    if (mode > 7) {
-        mode = 0;
+        if (global.TextDecoder) {
+            // available in gjs >= 1.70 (GNOME Shell >= 42)
+            this.byteArrayToString = (new TextDecoder().decode);
+        } else {
+            // gjs-specific, imports.byteArray is still available in (GNOME Shell 45 >=) but discouraged!
+            this.byteArrayToString = imports.byteArray.toString;
+        }
     }
-    settings.set_int('mode', mode);
-    parseStat(true);
-}
 
-function parseStat(forceDot = false) {
-    try {
-        let input_file = Gio.file_new_for_path('/proc/diskstats');
+    changeMode() {
+        this.mode++;
+        if (this.mode > 7) {
+            this.mode = 0;
+        }
+        this.settings.set_int('mode', this.mode);
+        this.parseStat(true);
+    }
 
-        let [, contents, etag] = input_file.load_contents(null);
-        contents = byteArrayToString(contents);
-        let lines = contents.split('\n');
+    parseStat(forceDot = false) {
+        try {
+            let input_file = Gio.file_new_for_path('/proc/diskstats');
 
-        let count = 0;
-        let line;
+            let [, contents, _etag] = input_file.load_contents(null);
+            contents = this.byteArrayToString(contents);
+            let lines = contents.split('\n');
 
-        for (let i=0;i<lines.length;i++) {
-            line = lines[i];
-            let fields = line.split(/ +/);
-            if (fields.length<=2) break;
+            let count = 0;
+            let line;
 
-            if (parseInt(fields[2])%16 === 0
+            for (let i = 0; i < lines.length; i++) {
+                line = lines[i];
+                let fields = line.split(/ +/);
+                if (fields.length <= 2) break;
+
+                if (parseInt(fields[2]) % 16 === 0
                     && fields[3].indexOf('md0') != 0
                     && fields[3].indexOf('ram0') != 0
                     && fields[3].indexOf('dm-0') != 0
                     && fields[3].indexOf('zram0') != 0
                     && fields[3].indexOf('loop0') != 0) {
-                count = count + parseInt(fields[6]) + parseInt(fields[10]);
-                // log(fields[3] + ':' + fields[6] + ' ' + fields[10] + ' ' + count);
+                    count = count + parseInt(fields[6]) + parseInt(fields[10]);
+                    // log(fields[3] + ':' + fields[6] + ' ' + fields[10] + ' ' + count);
+                }
+
             }
 
-        }
+            if (this.lastCount === 0) this.lastCount = count;
 
-        if (lastCount === 0) lastCount = count;
+            let speed = (count - this.lastCount) / this.refreshTime * 512;
 
-        let speed = (count - lastCount) / refreshTime * 512;
-
-        let dot = " ";
-        if (speed > lastSpeed || forceDot || speed > ledThreshold) {
-            if (speed > ledMinThreshold) {
-                if (mode == 0 || mode == 2 || mode == 4 || mode == 6 ) {
-                    dot = "●";
-                } else if (mode == 1 || mode == 3 || mode == 7 ) {
-                    dot = "⬤";
+            let dot = " ";
+            if (speed > this.lastSpeed || forceDot || speed > this.ledThreshold) {
+                if (speed > this.ledMinThreshold) {
+                    if (this.mode == 0 || this.mode == 2 || this.mode == 4 || this.mode == 6) {
+                        dot = "●";
+                    } else if (this.mode == 1 || this.mode == 3 || this.mode == 7) {
+                        dot = "⬤";
+                    }
                 }
             }
-        }
-        if (mode == 2 || mode == 3 || mode == 6 || mode == 7) {
-            ioSpeed.hide();
-        } else {
-            ioSpeed.show();
-        }
-        if (mode == 4 || mode == 5 || mode == 6 || mode == 7) {
-            ioSpeedStaticIcon.hide();
-        } else {
-            ioSpeedStaticIcon.show();
-        }
-        if (mode == 5) {
-            ioSpeedIcon.hide();
-        } else {
-            ioSpeedIcon.show();
+            if (this.mode == 2 || this.mode == 3 || this.mode == 6 || this.mode == 7) {
+                this.ioSpeed.hide();
+            } else {
+                this.ioSpeed.show();
+            }
+            if (this.mode == 4 || this.mode == 5 || this.mode == 6 || this.mode == 7) {
+                this.ioSpeedStaticIcon.hide();
+            } else {
+                this.ioSpeedStaticIcon.show();
+            }
+            if (this.mode == 5) {
+                this.ioSpeedIcon.hide();
+            } else {
+                this.ioSpeedIcon.show();
+            }
+
+            this.ioSpeedIcon.set_text(dot);
+            this.ioSpeed.set_text(this.speedToString(speed));
+
+            this.lastCount = count;
+            this.lastSpeed = speed;
+        } catch (e) {
+            this.ioSpeed.set_text(e.message);
         }
 
-        ioSpeedIcon.set_text(dot);
-        ioSpeed.set_text(speedToString(speed));
+        /*
+        let curDiskstats = GLib.file_get_contents('/proc/diskstats');
+    
+        if (diskstats == curDiskstats) {
+            if (cur !== 0) {
+                button.set_child(iconDark);
+                cur = 0;
+            }
+        } else {
+            if (cur != 1) {
+                button.set_child(icon);
+                cur = 1;
+            }
+            diskstats = curDiskstats;
+        }*/
 
-        lastCount = count;
-        lastSpeed = speed;
-    } catch (e) {
-        ioSpeed.set_text(e.message);
+        return true;
     }
 
-    /*
-    let curDiskstats = GLib.file_get_contents('/proc/diskstats');
+    speedToString(amount) {
+        let digits;
+        let speed_map;
+        speed_map = ["B/s", "K/s", "M/s", "G/s"];
 
-    if (diskstats == curDiskstats) {
-        if (cur !== 0) {
-            button.set_child(iconDark);
-            cur = 0;
+        if (amount === 0)
+            return "0" + speed_map[0];
+
+        let unit = 0;
+        while (amount >= 1000) { // 1M=1024K, 1MB/s=1000MB/s
+            amount /= 1000;
+            ++unit;
         }
-    } else {
-        if (cur != 1) {
-            button.set_child(icon);
-            cur = 1;
-        }
-        diskstats = curDiskstats;
-    }*/
 
-    return true;
-}
-
-function speedToString(amount) {
-    let digits;
-    let speed_map;
-    speed_map = ["B/s", "K/s", "M/s", "G/s"];
-
-    if (amount === 0)
-        return "0"  + speed_map[0];
-
-    let unit = 0;
-    while (amount >= 1000) { // 1M=1024K, 1MB/s=1000MB/s
-        amount /= 1000;
-        ++unit;
+        if (amount >= 100) // 100MB 100KB 200KB
+            digits = 0;
+        else if (amount >= 10) // 10MB 10.2
+            digits = 1;
+        else
+            digits = 2;
+        return String(amount.toFixed(digits)) + speed_map[unit];
     }
 
-    if (amount >= 100) // 100MB 100KB 200KB
-        digits = 0;
-    else if (amount >= 10) // 10MB 10.2
-        digits = 1;
-    else
-        digits = 2;
-    return String(amount.toFixed(digits)) + speed_map[unit];
-}
+    enable() {
+        this.init()
+        this.settings = this.getSettings(this.PREFS_SCHEMA);
 
-function enable() {
-    settings = ExtensionUtils.getSettings(PREFS_SCHEMA);
+        this.mode = this.settings.get_int('mode'); // default mode
 
-    mode = settings.get_int('mode'); // default mode
+        this.button = new St.Button({
+            style_class: 'panel-button',
+            reactive: true,
+            can_focus: true,
+            x_expand: true,
+            y_expand: false,
+            track_hover: true
+        });
 
-    button = new St.Button({
-        style_class: 'panel-button',
-        reactive: true,
-        can_focus: true,
-        x_expand: true,
-        y_expand: false,
-        track_hover: true
-    });
+        this.layoutManager = new St.BoxLayout({
+            vertical: false,
+            style_class: 'harddiskled-container'
+        });
 
-    layoutManager = new St.BoxLayout({
-        vertical: false,
-        style_class: 'harddiskled-container'});
+        this.ioSpeedStaticIcon = new St.Icon({
+            style_class: 'system-status-icon',
+            y_align: Clutter.ActorAlign.CENTER,
+            gicon: Gio.icon_new_for_string('drive-harddisk-symbolic')
+        });
 
-    /*
-    icon = new St.Icon({
-        gicon: Gio.icon_new_for_string(Me.path + "/icons/harddisk.svg")
-    });
-    iconDark = new St.Icon({
-        gicon: Gio.icon_new_for_string(Me.path + "/icons/harddisk-dark.svg")
-    });*/
+        this.ioSpeed = new St.Label({
+            text: '---',
+            y_align: Clutter.ActorAlign.CENTER,
+            style_class: 'harddiskled-label'
+        });
 
-    ioSpeedStaticIcon = new St.Icon({
-        style_class: 'system-status-icon',
-        y_align: Clutter.ActorAlign.CENTER,
-        gicon: Gio.icon_new_for_string('drive-harddisk-symbolic')
-    });
+        this.ioSpeedIcon = new St.Label({
+            text: '',
+            y_align: Clutter.ActorAlign.CENTER,
+            style_class: 'harddiskled-icon'
+        });
 
-    ioSpeed = new St.Label({
-        text: '---',
-        y_align: Clutter.ActorAlign.CENTER,
-        style_class: 'harddiskled-label'
-    });
+        this.layoutManager.add(this.ioSpeedStaticIcon);
+        this.layoutManager.add(this.ioSpeedIcon);
+        this.layoutManager.add(this.ioSpeed);
+        this.button.connect('button-press-event', this.changeMode.bind(this));
 
-    ioSpeedIcon = new St.Label({
-        text: '',
-        y_align: Clutter.ActorAlign.CENTER,
-        style_class: 'harddiskled-icon'
-    });
+        this.button.set_child(this.layoutManager);
 
-    layoutManager.add(ioSpeedStaticIcon);
-    layoutManager.add(ioSpeedIcon);
-    layoutManager.add(ioSpeed);
-    button.connect('button-press-event', changeMode);
+        Main.panel._rightBox.insert_child_at_index(this.button, 0);
+        this.timeout = Mainloop.timeout_add_seconds(this.refreshTime, this.parseStat.bind(this));
+    }
 
-    button.set_child(layoutManager);
-
-    Main.panel._rightBox.insert_child_at_index(button, 0);
-    timeout = Mainloop.timeout_add_seconds(refreshTime, parseStat);
-}
-
-function disable() {
-    Mainloop.source_remove(timeout);
-    timeout = null;
-    Main.panel._rightBox.remove_child(button);
-    button.destroy();
-    settings = button = layoutManager = ioSpeedStaticIcon = ioSpeed = ioSpeedIcon = null;
+    disable() {
+        Mainloop.source_remove(this.timeout);
+        this.timeout = null;
+        Main.panel._rightBox.remove_child(this.button);
+        this.button.destroy();
+        this.settings = this.button = this.layoutManager = this.ioSpeedStaticIcon = this.ioSpeed = this.ioSpeedIcon = null;
+    }
 }
